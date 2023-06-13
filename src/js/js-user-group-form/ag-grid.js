a2d90bd937d5f9854a831cef4377ae634a808f0b
Ron Dernedde <fidp21n.dernedde@mm-bbs.de>
2023-06-13 19:56:18 +0200

    updated form

--- src/js/js-user-group-form/ag-grid.js
+++ src/js/js-user-group-form/ag-grid.js
@@ -105,12 +105,67 @@ function deleteButtonRenderer(params) {
     return button;
   }
 
-// display tables
-document.addEventListener("DOMContentLoaded", () => {
-    const gridDivOne = document.querySelector("#all-users");
-    new agGrid.Grid(gridDivOne, gridOptions);
+function insertCommasAtIndices(string, indices) {
+  let result = string;
+  
+  for (let i = indices.length - 1; i >= 0; i--) {
+      const index = indices[i];
+      result = result.slice(0, index) + ',' + result.slice(index);
+  }
+  
+  return result;
+}
 
-    const gridDivTwo = document.querySelector("#all-users-selected");
-    new agGrid.Grid(gridDivTwo, gridOptionsTwo);
+// text-boy functions
+const textareaElement = document.getElementById('textarea');
+const addToTableButtonElement = document.getElementById('add-table-button');
+const addSeperatorButtonElement = document.getElementById('add-seperator-button');
+
+// add seperator 
+addSeperatorButtonElement.addEventListener('click', () => {
+  const textareaInput = textareaElement.value;
+
+  // search for endings in string and safe indexes
+  const searchStrings = ['.com', 'co.uk', '.be'];
+  const indexes = [];
+  searchStrings.forEach(element => {
+      indexes.push(...[...textareaInput.matchAll(new RegExp(element, 'gi'))].map(a => a.index + element.length));     
+  });
+  indexes.sort((a, b) => a - b);
+
+  // insert commas into string
+  let seperatedString = insertCommasAtIndices(textareaInput, indexes);
+  seperatedString = seperatedString.slice(0, -1);
+
+  // write value comma separted to array
+  let emailsAddresses = seperatedString.split(',');
+
+  // Get the current row data from the grid
+  const rowData = gridOptionsTwo.api.getDataAsCsv({ skipColumnHeader: true });
+
+  // Determine the starting ID for the new rows
+  const startingId = rowData.length > 0 ? rowData[rowData.length - 1].id + 1 : 1;
+
+  // Create new row data from the array
+  const newRowData = emailsAddresses.map((value, index) => ({
+      id: startingId + index,
+      username: value,
+      siterole: '' 
+  }));
+
+  // Add the new row data to the grid
+  const transaction = {
+    add: newRowData
+  };
+  
+  gridOptionsTwo.api.applyTransaction(transaction);
 });
 
+// display tables
+document.addEventListener("DOMContentLoaded", () => {
+  const gridDivOne = document.querySelector("#all-users");
+  new agGrid.Grid(gridDivOne, gridOptions);
+
+  const gridDivTwo = document.querySelector("#all-users-selected");
+  new agGrid.Grid(gridDivTwo, gridOptionsTwo);
+});
--- src/js/js-user-group-form/user-group-form.js
+++ src/js/js-user-group-form/user-group-form.js
@@ -1,23 +1,56 @@
-const openUserModalButton = document.getElementById('text-data-button');
-const closeUserModalButton = document.getElementById('close-button-text');
-const userModal = document.getElementById('modal-text');
 
-openUserModalButton.addEventListener('click', () => {
-    userModal.show();
+document.addEventListener('DOMContentLoaded', () => {
+    const openUserModalButton = document.getElementById('text-data-button');
+    const closeUserModalButton = document.getElementById('close-button-text');
+    const userModal = document.getElementById('modal-text');
+
+    openUserModalButton.addEventListener('click', () => {
+        userModal.show();
+        userModal.classList.add('show-model');
+    });
+
+    closeUserModalButton.addEventListener('click', () => {
+        userModal.close();
+    });
 });
 
-closeUserModalButton.addEventListener('click', () => {
-    userModal.close();
+
+document.addEventListener('DOMContentLoaded', () => {
+    const openTextModalButton = document.getElementById('open-button-user');
+    const closeTextModalButton = document.getElementById('close-button-user');
+    const textModal = document.getElementById('modal-user');
+
+    openTextModalButton.addEventListener('click', () => {
+        textModal.show();
+        textModal.classList.add('show-model');
+        textModal.style.flex = 'flex';
+    });
+
+    closeTextModalButton.addEventListener('click', () => {
+        textModal.close();
+        textModal.style.flex = 'none';
+    });
 });
 
-const openTextModalButton = document.getElementById('open-button-user');
-const closeTextModalButton = document.getElementById('close-button-user');
-const textModal = document.getElementById('modal-user');
-
-openTextModalButton.addEventListener('click', () => {
-    textModal.show();
+const uploadButton = document.getElementById('excel-data-button');
+const fileInput = document.getElementById('file-input');
+uploadButton.addEventListener('click', () => {
+    fileInput.click();
 });
 
-closeTextModalButton.addEventListener('click', () => {
-    textModal.close();
-});
+fileInput.addEventListener('change', handleFileUpload);
+
+function handleFileUpload(event) {
+    const file = event.target.files[0];
+
+    // Validate if a file was selected
+    if (file) {
+        // Perform further processing with the file
+        console.log('Selected file:', file.name);
+        // You can use libraries like XLSX.js or SheetJS to parse the Excel file
+        // and extract data as needed.
+    }
+}
+
+
+  
